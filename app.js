b724e7ce4f2a5fa4da58dd2ecf45ca5d189351ae
amol0900 <a.olsson59@gmail.com>
2019-11-06 11:27:27 +0100

    kristian

--- app.js
+++ app.js
@@ -5,24 +5,48 @@ var quill = new Quill('#editor', {
 });
 
 const editor = {}
-editor.noteText = document.querySelector('#editor .ql-editor');
+editor.noteText = document.querySelector('#editor');
 editor.addButton = document.querySelector('#formAddButton');
 /* form.color = document.querySelector('#formColor'); */
 
-const notes = document.querySelector('#notes');
+const notes = document.querySelector('.notes ul');
+
+/*
+let note = {
+    id: Date.now(),
+    text: "...html" // quill.root.innerHTML,
+    favourite: false,
+    deleted: false
+} */
+
+// let notes = [note,note,note]
 
 // Functions
+
+function newAddNote() {
+    let note = {
+        id: Date.now(),
+        text: quill.root.innerHTML
+    }
+    //console.log(quill.root.innerHTML)
+    //console.log(quill.getContents())
+    return (note);
+}
+
 function addNote() {
     let text = editor.noteText.value;
     let note = document.createElement('div');
+    let li = document.createElement('li');
     let deleteButton = document.createElement('span');
 
+    let newnote = newAddNote();
     note.classList.add('note');
     /* note.classList.add(editor.color.value); */
-    note.innerHTML = `<div class='note-text'>${text}</div>`;
+    note.innerHTML = `<div class='note-text'>${newnote.text}</div>`;
     deleteButton.classList.add('note-delete');
     deleteButton.innerHTML = '&times;';
 
+    note.appendChild(li);
     note.appendChild(deleteButton);
     notes.appendChild(note);
 
