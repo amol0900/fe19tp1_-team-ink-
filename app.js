00615b63e59bd7981f9154efa251099fab1fb1a8
amol0900 <a.olsson59@gmail.com>
2019-12-04 16:15:17 +0100

    fixes

--- app.js
+++ app.js
@@ -44,12 +44,12 @@ justHtmlContent.addEventListener('click', function (e) {
 
     clickedLI.remove();
     editor.setText('');
-    document.getElementById('square').value = '';
-    document.getElementById('square').focus();
+    document.getElementById('title').value = '';
+    document.getElementById('title').focus();
     saveNotes();
     selectedNote = null;
   } else {
-    var myTitle2 = document.getElementById('square');
+    var myTitle2 = document.getElementById('title');
     editor.setContents(selectedNote.content);
     myTitle2.value = selectedNote.title;
   }
